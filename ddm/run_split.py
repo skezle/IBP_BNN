4229a922a450bffd5c70b40a6133245a01a204bb
samk <kessler.s.c@gmail.com>
2019-08-23 09:51:33 +0100

    adds 5 runs average

--- ddm/run_split.py
+++ ddm/run_split.py
@@ -3,10 +3,14 @@ import tensorflow as tf
 import gzip
 import pickle
 import sys
-from ddm.alg.vcl import run_vcl, run_vcl_ibp
-from ddm.alg.coreset import rand_from_batch, k_center
-from ddm.alg.utils import plot
+from ddm.alg.vcl import run_vcl
+from ddm.alg.cla_models_multihead import Vanilla_NN, MFVI_IBP_NN
+from ddm.alg.utils import concatenate_results, get_scores
 from copy import deepcopy
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 
 class SplitMnistGenerator():
     def __init__(self, val=False, num_tasks=5):
@@ -79,44 +83,112 @@ class SplitMnistGenerator():
                 return next_x_train, next_y_train, next_x_test, next_y_test
 
 if __name__ == "__main__":
-    hidden_size = [256, 256]
-    batch_size = None
-    no_epochs = 100
-    single_head = False
+    seeds = [12, 13, 14, 15, 16]
 
-    # Run vanilla VCL
-    tf.set_random_seed(12)
-    np.random.seed(1)
+    vcl_ibp_accs = np.zeros((len(seeds), 5, 5))
+    vcl_h5_accs = np.zeros((len(seeds), 5, 5))
+    vcl_h10_accs = np.zeros((len(seeds), 5, 5))
+    vcl_h50_accs = np.zeros((len(seeds), 5, 5))
 
-    coreset_size = 0
-    data_gen = SplitMnistGenerator()
-    vcl_result = run_vcl_ibp(hidden_size, no_epochs, data_gen,
-        rand_from_batch, coreset_size, batch_size, single_head) # runs loop of tasks, trains/re-trains vcl, evaluates vcl
-    print(vcl_result)
+    for i in range(len(seeds)):
+        s = seeds[i]
+        hidden_size = [100]
+        batch_size = 128
+        no_epochs = 500
+        ibp_samples = 10
 
-    # # Run random coreset VCL
-    # tf.reset_default_graph()
-    # tf.set_random_seed(12)
-    # np.random.seed(1)
-    #
-    # coreset_size = 40
-    # data_gen = SplitMnistGenerator()
-    # rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-    #     rand_from_batch, coreset_size, batch_size, single_head)
-    # print(rand_vcl_result)
-    #
-    # # Run k-center coreset VCL
-    # tf.reset_default_graph()
-    # tf.set_random_seed(12)
-    # np.random.seed(1)
-    #
-    # data_gen = SplitMnistGenerator()
-    # kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-    #     k_center, coreset_size, batch_size, single_head)
-    # print(kcen_vcl_result)
+        # Run vanilla VCL
+        tf.set_random_seed(s)
+        np.random.seed(1)
 
-    # Plot average accuracy
-    vcl_avg = np.nanmean(vcl_result, 1)
-    # rand_vcl_avg = np.nanmean(rand_vcl_result, 1)
-    # kcen_vcl_avg = np.nanmean(kcen_vcl_result, 1)
-    plot('results/split.jpg', vcl_avg, rand_vcl_avg, kcen_vcl_avg)
+        ibp_acc = np.array([])
+
+        coreset_size = 0
+        data_gen = SplitMnistGenerator(val=False)
+        single_head = False
+        in_dim, out_dim = data_gen.get_dims()
+        x_testsets, y_testsets = [], []
+        for task_id in range(data_gen.max_iter):
+
+            tf.reset_default_graph()
+            x_train, y_train, x_test, y_test, _, _ = data_gen.next_task()
+            x_testsets.append(x_test)
+            y_testsets.append(y_test)
+
+            # Set the readout head to train
+            head = 0 if single_head else task_id
+            bsize = x_train.shape[0] if (batch_size is None) else batch_size
+
+            # Train network with maximum likelihood to initialize first model
+            if task_id == 0:
+                ml_model = Vanilla_NN(in_dim, hidden_size, out_dim, x_train.shape[0])
+                ml_model.train(x_train, y_train, task_id, no_epochs, bsize)
+                mf_weights = ml_model.get_weights()
+                mf_variances = None
+                mf_betas = None
+                ml_model.close_session()
+
+            # Train on non-coreset data
+            mf_model = MFVI_IBP_NN(in_dim, hidden_size, out_dim, x_train.shape[0], num_ibp_samples=ibp_samples,
+                                   prev_means=mf_weights,
+                                   prev_log_variances=mf_variances, prev_betas=mf_betas,
+                                   alpha0=5., beta0=1.,
+                                   learning_rate=0.0001, lambda_1=1.0, lambda_2=1.0, no_pred_samples=100)
+
+            mf_model.train(x_train, y_train, head, no_epochs, bsize,
+                           anneal_rate=0.0, min_temp=1.0)
+            mf_weights, mf_variances, mf_betas = mf_model.get_weights()
+
+            acc = get_scores(mf_model, x_testsets, y_testsets, single_head)
+            ibp_acc = concatenate_results(acc, ibp_acc)
+            mf_model.close_session()
+        vcl_ibp_accs[i, :, :] = ibp_acc
+
+        # Run Vanilla VCL
+        # Comparison with other single layer neural networks
+        tf.reset_default_graph()
+        hidden_size = [10]
+        data_gen = SplitMnistGenerator(val=False)
+        vcl_result_h10 = run_vcl(hidden_size, no_epochs, data_gen,
+                                 lambda a: a, coreset_size, batch_size, single_head, val=True)
+        vcl_h10_accs[i, :, :] = vcl_result_h10
+
+        tf.reset_default_graph()
+        hidden_size = [5]
+        data_gen = SplitMnistGenerator(val=False)
+        vcl_result_h5 = run_vcl(hidden_size, no_epochs, data_gen,
+                                lambda a: a, coreset_size, batch_size, single_head, val=True)
+        vcl_h5_accs[i, :, :] = vcl_result_h5
+
+        tf.reset_default_graph()
+        hidden_size = [50]
+        data_gen = SplitMnistGenerator(val=False)
+        vcl_result_h50 = run_vcl(hidden_size, no_epochs, data_gen,
+                                 lambda a: a, coreset_size, batch_size, single_head, val=True)
+        vcl_h50_accs[i, :, :] = vcl_result_h50
+
+    _ibp_acc = np.nanmean(vcl_ibp_accs, (0, 1))
+    _vcl_result_h10 = np.nanmean(vcl_h10_accs, (0, 1))
+    _vcl_result_h5 = np.nanmean(vcl_h5_accs, (0, 1))
+    _vcl_result_h50 = np.nanmean(vcl_h50_accs, (0, 1))
+    fig = plt.figure(figsize=(7, 4))
+    ax = plt.gca()
+    plt.plot(np.arange(len(_ibp_acc)) + 1, _ibp_acc, label='VCL + IBP', marker='o')
+    plt.plot(np.arange(len(_ibp_acc)) + 1, _vcl_result_h10, label='VCL h10', marker='o')
+    plt.plot(np.arange(len(_ibp_acc)) + 1, _vcl_result_h5, label='VCL h5', marker='o')
+    plt.plot(np.arange(len(_ibp_acc)) + 1, _vcl_result_h50, label='VCL h50', marker='o')
+    ax.set_xticks(range(1, len(_ibp_acc) + 1))
+    ax.set_ylabel('Average accuracy')
+    ax.set_xlabel('\# tasks')
+    ax.set_title('Split Mnist')
+    ax.legend()
+    fig.savefig('split_mnist_accs.png', bbox_inches='tight')
+    plt.close()
+
+    with open('results/split_mnist_res5.pkl', 'wb') as input_file:
+        pickle.dump({'vcl_ibp': vcl_ibp_accs,
+                     'vcl_h10': vcl_h10_accs,
+                     'vcl_h5': vcl_h5_accs,
+                     'vcl_h50': vcl_h50_accs}, input_file)
+
+    print("Finished running.")
