7d09c749c4a1bdbab1e5bef3646f2ef8c989754c
samk <kessler.s.c@gmail.com>
2019-10-25 14:01:37 +0100

    removes pdb

--- ddm/alg/cla_models_multihead.py
+++ ddm/alg/cla_models_multihead.py
@@ -1107,12 +1107,12 @@ class MFVI_IBP_NN(Cla_NN):
         """
         mc_samples = [self.prediction_prob(x_test, task_idx) for _ in range(10)]
         mc_samples_ar = np.concatenate(mc_samples, axis=0)
-        pdb.set_trace()
+        #pdb.set_trace()
         eps = 1e-16
         expected_p = np.mean(mc_samples_ar, axis=0)
-        predictive_entropy = -np.sum(expected_p * np.log(expected_p), axis=-1)
+        predictive_entropy = -np.sum(expected_p * np.log(expected_p), axis=-1) # (test_set_size, )
         mc_entropy = np.sum(mc_samples_ar * np.log(mc_samples_ar), axis=-1)
-        expected_entropy = -np.mean(mc_entropy, axis=0)
+        expected_entropy = -np.mean(mc_entropy, axis=0) # (test_set_size, )
         mi = np.mean(predictive_entropy - expected_entropy)
         return mi
 
