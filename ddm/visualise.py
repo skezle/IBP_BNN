aa6d5d45e180cf3a1e59afae79d20db082467577
samk <kessler.s.c@gmail.com>
2019-11-04 11:13:57 +0000

    updtaes plotting functions

--- ddm/visualise.py
+++ ddm/visualise.py
@@ -28,30 +28,34 @@ def plot_uncertainties(num_tasks, all_ibp_uncerts, all_vcl_h5_uncerts, all_vcl_h
     tick_size = 14
     legend_size = 16
 
-    fig, ax = plt.subplots(1, 4, figsize=(16, 5))
+    fig, ax = plt.subplots(1, 4, figsize=(16, 3))
     for i in range(num_tasks):
         ax[0].plot(np.arange(len(all_ibp_uncerts_norm[i, :])) + 1, all_ibp_uncerts_norm[i, :],
                    label='Task {}'.format(i),
                    marker='o', linewidth=lw)
-        ax[0].set_xticks(range(1, len(all_ibp_uncerts_norm[i, :])))
+        ax[0].set_xticks(range(1, len(all_ibp_uncerts_norm[i, :]) + 1))
+        ax[0].set_title("IBP + VCL")
 
     for i in range(num_tasks):
         ax[1].plot(np.arange(len(all_vcl_h5_uncerts_norm[i, :])) + 1, all_vcl_h5_uncerts_norm[i, :],
                    label='Task {}'.format(i),
                    marker='o', linewidth=lw)
-        ax[1].set_xticks(range(1, len(all_vcl_h5_uncerts_norm[i, :])))
+        ax[1].set_xticks(range(1, len(all_vcl_h5_uncerts_norm[i, :]) + 1))
+        ax[1].set_title("VCL h5")
 
     for i in range(num_tasks):
         ax[2].plot(np.arange(len(all_vcl_h10_uncerts_norm[i, :])) + 1, all_vcl_h10_uncerts_norm[i, :],
                    label='Task {}'.format(i),
                    marker='o', linewidth=lw)
-        ax[2].set_xticks(range(1, len(all_vcl_h10_uncerts_norm[i, :])))
+        ax[2].set_xticks(range(1, len(all_vcl_h10_uncerts_norm[i, :]) + 1))
+        ax[2].set_title("VCL h10")
 
     for i in range(num_tasks):
         ax[3].plot(np.arange(len(all_vcl_h50_uncerts_norm[i, :])) + 1, all_vcl_h50_uncerts_norm[i, :],
                    label='Task {}'.format(i),
                    marker='o', linewidth=lw)
-        ax[3].set_xticks(range(1, len(all_vcl_h50_uncerts_norm[i, :])))
+        ax[3].set_xticks(range(1, len(all_vcl_h50_uncerts_norm[i, :]) + 1))
+        ax[3].set_title("VCL h50")
 
     for i in range(num_results):
         ax[i].set_xlabel('Tasks', fontsize=legend_size)
@@ -66,29 +70,57 @@ def plot_uncertainties(num_tasks, all_ibp_uncerts, all_vcl_h5_uncerts, all_vcl_h
 
 def plot_Zs(num_tasks, num_layers, Zs, dataset, tag):
     # this is plotting the final run Zs
+    lw = 2
+
+    grid_color = '0.1'
+    grid_lw = 0.2
+
+    title_size = 16
+    label_size = 16
+    tick_size = 12
+    legend_size = 12
     if num_layers == 1:
         fig, ax = plt.subplots(2, num_tasks, figsize=(16, 4))
         for i in range(num_tasks):
-            ax[0][i].imshow(np.squeeze(Zs[i])[:50, :], cmap=plt.cm.Greys_r, vmin=0, vmax=1)
+            imgplot = ax[0][i].imshow(np.squeeze(Zs[i])[:50, :], cmap=plt.cm.Greys, vmin=0, vmax=1)
+            ax[0][i].set_xticks(np.arange(0.0, 100, step=50))
+            ax[0][i].set_xlabel('$k$', fontsize=legend_size)
             ax[0][i].set_xticklabels([])
             ax[0][i].set_yticklabels([])
-            ax[1][i].hist(np.sum(np.squeeze(Zs[i]), axis=1), 10)
+            ax[1][i].hist(np.sum(np.asarray(np.squeeze(Zs[i] > 0.1)).astype(int), axis=1), 8, alpha=0.7,
+                          edgecolor='green',
+                          linewidth=1.5)
             ax[1][i].set_yticklabels([])
             ax[1][i].set_xlabel("Task {}".format(i + 1))
+            if i == 4:
+                cbar_ax = fig.add_axes([0.92, 0.57, 0.01, 0.28])
+                fig.colorbar(imgplot, cax=cbar_ax)
     elif num_layers == 2:
-        fig, ax = plt.subplots(4, num_tasks, figsize=(16, 4))
+        fig, ax = plt.subplots(4, num_tasks, figsize=(16, 8))
         for i in range(num_tasks):
-            ax[0][i].imshow(np.squeeze(Zs[2*i])[:50, :], cmap=plt.cm.Greys_r, vmin=0, vmax=1)
+            imgplot1 = ax[0][i].imshow(np.squeeze(Zs[2*i])[:50, :], cmap=plt.cm.Greys, vmin=0, vmax=1)
+            ax[0][i].set_xticks(np.arange(0.0, 100, step=50))
+            ax[0][i].set_xlabel('$k$', fontsize=legend_size)
             ax[0][i].set_xticklabels([])
             ax[0][i].set_yticklabels([])
-            ax[1][i].imshow(np.squeeze(Zs[2*i + 1])[:50, :], cmap=plt.cm.Greys_r, vmin=0, vmax=1)
-            ax[1][i].set_xticklabels([])
+            ax[1][i].hist(np.sum(np.asarray(np.squeeze(Zs[2*i] > 0.1)).astype(int), axis=1), 8, alpha=0.7,
+                          edgecolor='green',
+                          linewidth=1.5)
             ax[1][i].set_yticklabels([])
-            ax[2][i].hist(np.sum(np.squeeze(Zs[2*i]), axis=1), 10)
+            imgplot2 = ax[2][i].imshow(np.squeeze(Zs[2 * i + 1])[:50, :], cmap=plt.cm.Greys, vmin=0, vmax=1)
+            ax[2][i].set_xticks(np.arange(0.0, 100, step=50))
+            ax[2][i].set_xlabel('$k$', fontsize=legend_size)
+            ax[2][i].set_xticklabels([])
             ax[2][i].set_yticklabels([])
-            ax[3][i].hist(np.sum(np.squeeze(Zs[2*i + 1]), axis=1), 10)
+            ax[3][i].hist(np.sum(np.asarray(np.squeeze(Zs[2*i + 1] > 0.1)).astype(int), axis=1), 8, alpha=0.7,
+                          edgecolor='green',
+                          linewidth=1.5)
             ax[3][i].set_yticklabels([])
             ax[3][i].set_xlabel("Task {}".format(i + 1))
+            if i == 4:
+                cbar_ax = fig.add_axes([0.92, 0.57, 0.01, 0.28])
+                fig.colorbar(imgplot1, cax=cbar_ax)
+                fig.colorbar(imgplot2, cax=cbar_ax)
     else:
         raise ValueError
     plt.savefig('plots/Zs_{0}_mnist_{1}.pdf'.format(dataset, tag), bbox_inches='tight')
